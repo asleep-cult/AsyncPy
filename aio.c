e8e744d90bb02e04d98c951f58cf00880b2279a1
asleep-cult <asleep.cult@gmail.com>
2021-02-03 00:48:26 -0500

    Added aio.AioRequest.cancel and aio.suspend (basically select) and error handling

--- aio.c
+++ aio.c
@@ -13,25 +13,37 @@ enum AioRequest_Type {
 
 typedef struct AioRequest {
         PyObject_HEAD;
+        int fd;
         int req_type;
         int usable;
         struct aiocb *aiocbp;
 } AioRequest;
 
+static PyObject *AioRequest_GetResult(PyObject *self, PyObject *args);
+PyObject *AioRequest_Cancel(PyObject *self, PyObject *args);
+
+static PyMethodDef AioRequest_Methods {
+        {"get_result", AioRequest_GetResult, METH_VARARGS, NULL},
+        {"cancel", AioRequest_Cancel, METH_VARARGS, NULL},
+        {NULL, NULL, 0, NULL}        /* Sentinel */
+};
+
 static PyTypeObject AioRequest_TypeObject = {
         PyVarObject_HEAD_INIT(NULL, 0)
         "AioRequest",
-        sizeof(AioRequest)
+        sizeof(AioRequest),
+        .tp_methods = AioRequest_Methods
 };
 
-static AioRequest *make_aiorequest()
+static AioRequest *make_aiorequest(int fd)
 {
 	AioRequest *request;
 	request = PyObject_GC_New(AioRequest, &AioRequest_TypeObject);
-
+        request->fd = fd;
 	request->aiocbp = malloc(sizeof(struct aiocb));
         request->usable = 1;
 	request->aiocbp->aio_reqprio = 0;
+        request->aiocbp->aio_fildes = fd;
 	request->aiocbp->aio_sigevent.sigev_notify = SIGEV_SIGNAL;
 	request->aiocbp->aio_sigevent.sigev_signo = SIGUSR1;
 	request->aiocbp->aio_sigevent.sigev_value.sival_ptr = NULL;
@@ -44,9 +56,8 @@ static PyObject *AioRequest_Read(PyObject *self, PyObject *args)
         int fd = PyLong_AsLong(PyTuple_GetItem(args, 0));
         int bufsize = PyLong_AsLong(PyTuple_GetItem(args, 1));
 
-        AioRequest *request = make_aiorequest();
+        AioRequest *request = make_aiorequest(fd);
         request->req_type = READ;
-        request->aiocbp->aio_fildes = fd;
         request->aiocbp->aio_buf = malloc(sizeof(bufsize));
         request->aiocbp->aio_nbytes = bufsize;
 
@@ -62,9 +73,8 @@ static PyObject *AioRequest_Write(PyObject *self, PyObject *args)
         char *buffer = PyBytes_AsString(bytes);
         Py_ssize_t bufsize = PyBytes_Size(bytes);
 
-        AioRequest *request = make_aiorequest();
+        AioRequest *request = make_aiorequest(fd);
         request->req_type = WRITE;
-        request->aiocbp->aio_fildes = fd;
         request->aiocbp->aio_buf = buffer;
         request->aiocbp->aio_nbytes = bufsize;
 
@@ -75,7 +85,7 @@ static PyObject *AioRequest_Write(PyObject *self, PyObject *args)
 
 static PyObject *AioRequest_GetResult(PyObject *self, PyObject *args)
 {
-        AioRequest *request = (AioRequest *)PyTuple_GetItem(args, 0);
+        AioRequest *request = (AioRequest *)self;
 
         if (!request->usable) {
                 PyErr_SetString(PyExc_ValueError, "Can\'t reuse requests");
@@ -83,21 +93,102 @@ static PyObject *AioRequest_GetResult(PyObject *self, PyObject *args)
         }
         request->usable = 0;
 
-        if (request->req_type == READ) {
-                char *buffer = (char *)request->aiocbp->aio_buf;
-                int buflen = request->aiocbp->aio_nbytes;
-                PyObject *bytes = PyBytes_FromStringAndSize(buffer, buflen);
-                return bytes;
+        int status = aio_error(request->aiocbp);
+        if (status != 0) {
+                if (status == EINPROGRESS) {
+                        PyErr_SetString(
+                                PyExc_BlockingIOError,
+                                "Can't get result from unsinished request");
+                }
+                else if (status == ECANCELED) {
+                        PyErr_SetString(
+                                PyExc_OSError,
+                                "Can't get result from cancelled request");
+                        )
+                }
+                else {
+                        PyErr_SetFromErrno(PyExc_OSError);
+                }
+                return NULL;
         }
 
-        PyErr_SetString(PyExc_ValueError, "can\'t do that yet");
-        return NULL;
+        int ret = aio_return(request->aiocbp);
+
+        if (ret == -1) {
+                PyErr_SetFromErrno(PyExc_OSError);
+        }
+
+        if (request->req_type == READ) {
+                char *buffer = (char *)request->aiocbp->aio_buf;
+                PyObject *bytes = PyBytes_FromStringAndSize(buffer, ret);
+                return bytes;
+        }
+        return ret;
+}
+
+PyObject *AioRequest_Cancel(PyObject *self, PyObject *args)
+{
+        PyObject *request = (AioRequest *)self;
+
+        int status = aio_cancel(request->aiobcp);
+
+        if (status != AIO_CANCELLED) {
+                if (status == AIO_NOTCANCELLED) {
+                        PyErr_SetString(
+                                PyExc_OSError,
+                                "Failed to cancel request");
+                }
+                else if (status == AIO_ALLDONE) {
+                        PyErr_SetString(
+                                PyExc_OSError,
+                                "The request is already finished");
+                }
+                else {
+                        PyErr_SetFromErrno(PyExc_OSError);
+                }
+                return NULL;
+        }
+        Py_RETURN_NONE;
+}
+
+static PyObject *Aio_Suspend(PyObject *self, PyObject *args)
+{
+        PyObject *requests = PyTuple_GetItem(args, 0);
+        long nanoseconds = PyLong_AsLong(PyTuple_GetItem(args, 1));
+
+        struct timespec *timeout;
+        if (nanoseconds == 0) {
+                struct timespec timeout_;
+                timeout_.tv_sec = 0;
+                timeout_.tv_nsec = nanoseconds;
+                timeout = &timeout_;
+        }
+        else {
+                timeout = NULL;
+        }
+
+        int length = PyList_Size(aiocbs);
+        const struct aiocb *const aiocb_list[length];
+
+        for (int i = 0; i < length; i++) {
+                AioRequest *request = (AioRequest *)PyList_GetItem(
+                                                                requests, i);
+                aiocb_list[i] = (const aiobc *)request->aiocbp;
+        }
+
+        int status = aio_suspend(aiocb_list, length, timeout);
+
+        if (status == -1) {
+                PyErr_SetFromErrno(PyExc_OSError);
+                return NULL;
+        }
+        Py_RETURN_NONE;
 }
 
 static PyMethodDef aiomethods[] = {
         {"read",  AioRequest_Read, METH_VARARGS, NULL},
         {"write", AioRequest_Write, METH_VARARGS, NULL},
-        {"get_result", AioRequest_GetResult, METH_VARARGS, NULL},
+        {"suspend", Aio_Suspend, METH_VARARGS, NULL},
         {NULL, NULL, 0, NULL}        /* Sentinel */
 };
 
