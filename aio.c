b508cf5bd45dded888c88aed67aad03a8486286d
asleep-cult <asleep.cult@gmail.com>
2021-02-03 01:19:10 -0500

    '???'

--- aio.c
+++ aio.c
@@ -176,7 +176,7 @@ static PyObject *Aio_Suspend(PyObject *self, PyObject *args)
         }
 
         int status = aio_suspend(
-                        (const struct aiocb *const aiocb_list*),
+                        (const(const struct aiocb *) *)aiocbp_list,
                         length,
                         timeout);
 
